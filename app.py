168ea1298ce9102d0b623a671fb06c1459fe7f0c
Peterhague <prhague@hotmail.co.uk>
2021-11-06 15:30:15 +0000

    added further dependencies from flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,12 +13,14 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMngo(app)
 
 @app.route("/")
+@app.route("/get_books")
 
-
-def hello():
-    return "Hello World!"
+def get_books():
+    books = mongo.db.books.find()
+    return render_template("books.html", books=books)
 
 
 if __name__ == "__main__":
