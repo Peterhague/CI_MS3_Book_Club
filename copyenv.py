154484c77d810c7db1374ebb122eb1fad8239b8a
Peterhague <prhague@hotmail.co.uk>
2021-11-06 21:31:34 +0000

    debugging base.html

---
+++ copyenv.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "TFDSFU11525")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://en0u0244:Eccw781459Do1@myfirstcluster.ukygd.mongodb.net/Book_Club?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "Book_Club")
