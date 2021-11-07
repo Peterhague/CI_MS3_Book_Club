601f4d520f0d7ff492f5f9a83e75a92cea33fdea
Peterhague <prhague@hotmail.co.uk>
2021-11-07 14:09:22 +0000

    debug app.py

--- app.py
+++ app.py
@@ -1,51 +1,51 @@
-import os
-from flask import (
-    Flask, flash, render_template, redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-if os.path.exists("env.py"):
-    import env
+# import os
+# from flask import (
+#     Flask, flash, render_template, redirect, request, session, url_for)
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+# from werkzeug.security import generate_password_hash, check_password_hash
+# if os.path.exists("env.py"):
+#     import env
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
-@app.route("/")
-@app.route("/see_books")
+# @app.route("/")
+# @app.route("/see_books")
 
-def see_books():
-    books = mongo.db.books.find()
-    return render_template("books.html", books=books)
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            flash("Username already exists")
-            return redirect(url_for("register"))
-
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
-
-        session["user"] = request.form.get("username").lower()
-        flash("Registration successful")
-    return render_template("register.html")
+# def see_books():
+#     books = mongo.db.books.find()
+#     return render_template("books.html", books=books)
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         existing_user = mongo.db.users.find_one(
+#             {"username": request.form.get("username").lower()})
+
+#         if existing_user:
+#             flash("Username already exists")
+#             return redirect(url_for("register"))
+
+#         register = {
+#             "username": request.form.get("username").lower(),
+#             "password": generate_password_hash(request.form.get("password"))
+#         }
+#         mongo.db.users.insert_one(register)
+
+#         session["user"] = request.form.get("username").lower()
+#         flash("Registration successful")
+#     return render_template("register.html")
+
+
+# if __name__ == "__main__":
+#     app.run(host=os.environ.get("IP"),
+#             port=int(os.environ.get("PORT")),
+#             debug=True)
