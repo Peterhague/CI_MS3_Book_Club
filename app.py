31797b2a114b0764f377a91fc7ca92f2cc9e6a6f
Peterhague <prhague@hotmail.co.uk>
2021-11-11 13:34:41 +0000

    changed member variable to joiner to try to fix bug

--- app.py
+++ app.py
@@ -142,10 +142,10 @@ def delete_book(book_id):
 @app.route("/join_club/<book_id>", methods=["GET", "POST"])
 def join_club(book_id):
     if request.method == "POST":
-        member = {
+        joiner = {
             "members": session["user"]
         }
-    mongo.db.books.update({"_id": ObjectId(book_id)}, member)
+    mongo.db.books.update({"_id": ObjectId(book_id)}, joiner)
     flash("You have joined this book club")
 
     book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
