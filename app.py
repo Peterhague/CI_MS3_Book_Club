acb253723c6297035247de80afa9b668c430a86f
Peterhague <prhague@hotmail.co.uk>
2021-11-13 18:23:48 +0000

    edited join function

--- app.py
+++ app.py
@@ -144,8 +144,8 @@ def delete_book(book_id):
 
 @app.route("/join_club/<book_id>", methods=["GET", "POST"])
 def join_club(book_id):
-    member = {"genre_name": "JOINED"}
-    mongo.db.books.set({"_id": ObjectId(book_id)}, member)
+    member = { "$set": {"genre_name": "JOINED"}}
+    mongo.db.books.update({"_id": ObjectId(book_id)}, member)
     flash("You have joined this book club")
 
     book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
