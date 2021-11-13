bf067aa8f275c45d69bbd5f409728952efa8c024
Peterhague <prhague@hotmail.co.uk>
2021-11-13 23:12:27 +0000

    added second function to join button

--- app.py
+++ app.py
@@ -147,7 +147,6 @@ def delete_book(book_id):
 @app.route("/join_club/<book_id>", methods=["GET", "POST"])
 def join_club(book_id):
     member = { "$addToSet": {"members": session["user"]}}
-    club = { "$addToSet": {"clubs_joined": book_id}}
     mongo.db.books.update({"_id": ObjectId(book_id)}, member)
     flash("You have joined this book club")
 
