f57b3c01bc517a9b0af29134ff0feb52a4694bf2
Peterhague <prhague@hotmail.co.uk>
2021-11-15 17:01:19 +0000

    added home page template and link for each book club

--- app.py
+++ app.py
@@ -192,7 +192,8 @@ def leave_club(book_id):
 @app.route("/go_to_club/<book_id>", methods=["GET", "POST"])
 def go_to_club(book_id):
     books = list(mongo.db.books.find())
-    return render_template("club_home.html", book_id=book_id, books=books,)
+    book = mongo.db.books.find_one({"_id": ObjectId(book_id)}
+    return render_template("club_home.html", book_id=book_id, book=book,)
 
 
 if __name__ == "__main__":
