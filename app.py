7a44192c9062163a0973bfbe31398174357272a6
Peterhague <prhague@hotmail.co.uk>
2021-11-15 20:41:15 +0000

    added flesh to comment function, for posting to db

--- app.py
+++ app.py
@@ -202,8 +202,7 @@ def add_comment(book_id):
         user_id = mongo.db.users.find_one({"username": session["user"]})["_id"]
         mongo.db.books.update({"_id": ObjectId(book_id)}, comment)
         mongo.db.users.update({"_id": ObjectId(user_id)}, comment)
-
-    book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
+        
     return redirect(url_for("club_home"))
 
 
