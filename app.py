7d7d4b2cc3395198d1b565b9540d227529f40388
Peterhague <prhague@hotmail.co.uk>
2021-11-16 22:24:37 +0000

    bug fixing edit_details function

--- app.py
+++ app.py
@@ -87,15 +87,16 @@ def login():
 def edit_details(this_user):
     if request.method == "POST":
         this_record = mongo.db.users.find_one({"username": this_user})
-        nominal = mongo.db.users.find_one({"username": session["user"]})["_id"]
+        user_id = mongo.db.users.find_one({"username": session["user"]})["_id"]        
+        pw = mongo.db.users.find_one({"username": session["user"]})["password"]
         edit_user = {
             "first_name": request.form.get("first_name"),
             "last_name": request.form.get("last_name"),
             "location": request.form.get("location"),
             "username": request.form.get("username"),
-            "password": nominal
+            "password": pw
         }
-        mongo.db.users.update({"_id": ObjectId(nominal)}, edit_user)
+        mongo.db.users.update({"_id": ObjectId(user_id)}, edit_user)
         flash("Account Successfully Updated")
 
     return render_template("edit_details.html", this_user=this_user)
